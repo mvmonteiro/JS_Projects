4f416954eeb52697435475a1987fc9e477de9b45
mvmonteiro <marcus.vinim@icloud.com>
2023-04-13 13:20:19 -0300

    fixing problem with range numbers.

--- Secret_Number/app/sort.js
+++ Secret_Number/app/sort.js
@@ -1,14 +1,20 @@
 const menorValor = 1;
 const maiorValor = 1000;
 const numeroSecreto = gerarNumeroAleatorio();
-const elementoMenorValor = document.getElementById("menor-valor");
-const elementoMaiorValor = document.getElementById("maior-valor");
+const elementoMenorValor = document.querySelectorAll("#menor-valor");
+const elementoMaiorValor = document.querySelectorAll("#maior-valor");
+
 
 function gerarNumeroAleatorio () {
     return parseInt(Math.random() * maiorValor + 1);
 }
 
-elementoMenorValor.innerHTML = menorValor;
-elementoMaiorValor.innerHTML = maiorValor;
+elementoMenorValor.forEach( e => {
+    e.innerHTML = menorValor;
+})
+
+elementoMaiorValor.forEach( e => {
+    e.innerHTML = maiorValor;
+})
 
 console.log(numeroSecreto);
